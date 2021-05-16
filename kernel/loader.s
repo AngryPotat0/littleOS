a4170675b3534758a0b8f82d9908ca84abd9a4c6
AngryPotat0 <211957142@qq.com>
2021-05-16 23:04:17 +0800

    grub

--- kernel/loader.s
+++ kernel/loader.s
@@ -1,5 +1,5 @@
 extern main
-[EXTERN	printMSG]
+; [EXTERN	printMSG]
 global loader                   ; the entry symbol for ELF
 
 MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
@@ -8,6 +8,12 @@ CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                 ; (magic number + checksum + flags should equal 0)
 KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
 
+section .__mbHeader
+align 0x4
+	dd MAGIC_NUMBER             ; write the magic number to the machine code,
+    dd FLAGS                    ; the flags,
+    dd CHECKSUM                 ; and the checksum
+
 section .bss
 align 4                                     ; align at 4 bytes
 kernel_stack:                               ; label points to beginning of memory
@@ -15,9 +21,7 @@ kernel_stack:                               ; label points to beginning of memor
 
 section .text:                  ; start of the text (code) section
 align 4                         ; the code must be 4 byte aligned
-    dd MAGIC_NUMBER             ; write the magic number to the machine code,
-    dd FLAGS                    ; the flags,
-    dd CHECKSUM                 ; and the checksum
+
 
 loader:                         ; the loader label (defined as entry point in linker script)
     mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
